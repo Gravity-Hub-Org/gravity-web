c11fc5108350118a8492c86a8a1ce0f4b2625ce1
Shamil Khalilov <shamfama@gmail.com>
2020-05-24 16:59:09 +0300

    Optimize images

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,35 +4,38 @@ module.exports = {
    */
   head: {
     title: 'Gravity Hub',
-    meta: [{
-        charset: 'utf-8'
+    meta: [
+      {
+        charset: 'utf-8',
       },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        content: 'width=device-width, initial-scale=1',
       },
       {
         hid: 'description',
         name: 'description',
-        content: 'gravity-hub site'
-      }
+        content: 'gravity-hub site',
+      },
     ],
-    link: [{
+    link: [
+      {
         rel: 'icon',
         type: 'image/x-icon',
-        href: '/favicon.ico'
+        href: '/favicon.ico',
       },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Orbitron:400,500,600,700,800,900|Poppins:100,200,300,400,500,600,700,800,900&display=swap'
-      }
-    ]
+        href:
+          'https://fonts.googleapis.com/css?family=Orbitron:400,500,600,700,800,900|Poppins:100,200,300,400,500,600,700,800,900&display=swap',
+      },
+    ],
   },
   /*
    ** Customize the progress bar color
    */
   loading: {
-    color: '#ff8e1e'
+    color: '#ff8e1e',
   },
   /*
    ** Build configuration
@@ -41,19 +44,21 @@ module.exports = {
     /*
      ** Run ESLint on save
      */
-    extend(config, {
-      isDev,
-      isClient
-    }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+          exclude: /(node_modules)/,
+        });
       }
-    }
+    },
   },
   srcDir: 'src/',
-}
+  buildModules: ['@aceforth/nuxt-optimized-images'],
+
+  optimizedImages: {
+    optimizeImages: true,
+  },
+};
