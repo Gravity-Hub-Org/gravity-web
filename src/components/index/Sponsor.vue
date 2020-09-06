7ede671ce745b7114a5bb1c9455fc61cf0ead256
PoulWalcker <pashasimanko123@gmail.com>
2020-09-06 21:16:56 +0300

    Add Sponsors paragraph

---
+++ src/components/index/Sponsor.vue
@@ -0,0 +1,294 @@
+<template lang="pug">
+section.sponsor-wrap 
+  .container-content-logo
+    .block-sponsor-gravity
+      h2.title-text Network Partnerships
+      .container-img-sponsors
+        .block
+          .sponsor-item
+            <img class='img-sponsors' src='img/swingby.svg', alt='SwingBy'>
+            .cotainer-about
+              .about-sponsor
+                <img class='img-sponsors' src='img/swingby.svg', alt='SwingBy'>
+                .text-about Swingby provides technology for inter-chain communications based on MPC (multiparty computations), TSS (threshold signatures) and ZK (zero-knowledge). This approach works with any chain and is most helpful with blockchains that lack Turing-complete smart contracts functionality.
+                .status Status:
+                  .status-type &nbsp; Validator &nbsp; <img src='~static/img/icons/Vector.svg'>
+                a.link(href='https://swingby.network') https://swingby.network
+                <img class='click' src='~static/img/icons/tap-copy-6.svg'>
+          .sponsor-item
+            <img class='img-sponsors' src='img/Logo_VL_2.svg', alt='VLab'>
+            .cotainer-about
+              .about-sponsor
+                <img class='img-sponsors' src='img/Logo_VL_2.svg', alt='VLab'>
+                .text-about Swingby provides technology for inter-chain communications based on MPC (multiparty computations), TSS (threshold signatures) and ZK (zero-knowledge). This approach works with any chain and is most helpful with blockchains that lack Turing-complete smart contracts functionality.
+                .status Status:
+                  .status-type &nbsp; Validator &nbsp; <img src='~static/img/icons/Vector.svg'>
+                a.link(href='#') Vlab
+                <img class='click' src='~static/img/icons/tap-copy-6.svg'>
+          .sponsor-item
+            <img class='img-sponsors' src='img/waves_enterprice-1.svg', alt='Waves-enterprice'>
+            .cotainer-about
+              .about-sponsor
+                <img class='img-sponsors' src='img/waves_enterprice-1.svg', alt='Waves-enterprice'>
+                .text-about Swingby provides technology for inter-chain communications based on MPC (multiparty computations), TSS (threshold signatures) and ZK (zero-knowledge). This approach works with any chain and is most helpful with blockchains that lack Turing-complete smart contracts functionality.
+                .status Status:
+                  .status-type &nbsp; Validator &nbsp; <img src='~static/img/icons/Vector.svg'>
+                a.link(href='#') Waves
+                <img class='click' src='~static/img/icons/tap-copy-6.svg'>
+          .sponsor-item
+            <img class='img-sponsors' src='img/waves_exchange-1.svg', alt='Waves-exchange'>
+            .cotainer-about
+              .about-sponsor
+                <img class='img-sponsors' src='img/waves_exchange-1.svg', alt='Waves-exchange'>
+                .text-about Swingby provides technology for inter-chain communications based on MPC (multiparty computations), TSS (threshold signatures) and ZK (zero-knowledge). This approach works with any chain and is most helpful with blockchains that lack Turing-complete smart contracts functionality.
+                .status Status:
+                  .status-type &nbsp; Validator &nbsp; <img src='~static/img/icons/Vector.svg'>
+                a.link(href='#') Waves
+                <img class='click' src='~static/img/icons/tap-copy-6.svg'>
+        .block
+          .sponsor-item
+            <img class='img-sponsors' src='img/1inch.svg', alt='1inch'>
+            .cotainer-about
+              .about-sponsor
+                <img class='img-sponsors' src='img/1inch.svg', alt='1inch'>
+                .text-about Swingby provides technology for inter-chain communications based on MPC (multiparty computations), TSS (threshold signatures) and ZK (zero-knowledge). This approach works with any chain and is most helpful with blockchains that lack Turing-complete smart contracts functionality.
+                .status Status:
+                  .status-type &nbsp; Validator &nbsp; <img src='~static/img/icons/Vector.svg'>
+                a.link(href='#') 1inch
+                <img class='click' src='~static/img/icons/tap-copy-6.svg'>
+          .sponsor-item
+            <img class='img-sponsors' src='img/Tron_logo-1.svg', alt='Tron'>
+            .cotainer-about
+              .about-sponsor
+                <img class='img-sponsors' src='img/Tron_logo-1.svg', alt='Tron'>
+                .text-about Swingby provides technology for inter-chain communications based on MPC (multiparty computations), TSS (threshold signatures) and ZK (zero-knowledge). This approach works with any chain and is most helpful with blockchains that lack Turing-complete smart contracts functionality.
+                .status Status:
+                  .status-type &nbsp; Validator &nbsp; <img src='~static/img/icons/Vector.svg'>
+                a.link(href='#') Tron
+                <img class='click' src='~static/img/icons/tap-copy-6.svg'>
+          .sponsor-item
+            <img class='img-sponsors' src='img/ontology-1.svg', alt='Ontology'>
+            .cotainer-about
+              .about-sponsor
+                <img class='img-sponsors' src='img/ontology-1.svg', alt='Ontology'>
+                .text-about Swingby provides technology for inter-chain communications based on MPC (multiparty computations), TSS (threshold signatures) and ZK (zero-knowledge). This approach works with any chain and is most helpful with blockchains that lack Turing-complete smart contracts functionality.
+                .status Status:
+                  .status-type &nbsp; Validator &nbsp; <img src='~static/img/icons/Vector.svg'>
+                a.link(href='#') Ontology
+                <img class='click' src='~static/img/icons/tap-copy-6.svg'>
+          .sponsor-item
+            <img class='img-sponsors' src='img/waves_association-1.svg', alt='Waves-associations'>
+            .cotainer-about
+              .about-sponsor
+                <img class='img-sponsors' src='img/waves_association-1.svg', alt='Waves-association'>
+                .text-about Swingby provides technology for inter-chain communications based on MPC (multiparty computations), TSS (threshold signatures) and ZK (zero-knowledge). This approach works with any chain and is most helpful with blockchains that lack Turing-complete smart contracts functionality.
+                .status Status:
+                  .status-type &nbsp; Validator &nbsp; <img src='~static/img/icons/Vector.svg'>
+                a.link(href='#') Waves
+                <img class='click' src='~static/img/icons/tap-copy-6.svg'>
+        .block
+          .sponsor-item
+            <img class='img-sponsors' src='img/fetch.svg', alt='Fetch'>
+            .cotainer-about
+              .about-sponsor
+                <img class='img-sponsors' src='img/fetch.svg', alt='Fetch'>
+                .text-about Swingby provides technology for inter-chain communications based on MPC (multiparty computations), TSS (threshold signatures) and ZK (zero-knowledge). This approach works with any chain and is most helpful with blockchains that lack Turing-complete smart contracts functionality.
+                .status Status:
+                  .status-type &nbsp; Validator &nbsp; <img src='~static/img/icons/Vector.svg'>
+                a.link(href='#') Fetch
+                <img class='click' src='~static/img/icons/tap-copy-6.svg'>
+          .sponsor-item
+            <img class='img-sponsors' src='img/neutrino-1.svg', alt='Neutrino'>
+            .cotainer-about
+              .about-sponsor
+                <img class='img-sponsors' src='img/neutrino-1.svg', alt='Neutrino'>
+                .text-about Swingby provides technology for inter-chain communications based on MPC (multiparty computations), TSS (threshold signatures) and ZK (zero-knowledge). This approach works with any chain and is most helpful with blockchains that lack Turing-complete smart contracts functionality.
+                .status Status:
+                  .status-type &nbsp; Validator &nbsp; <img src='~static/img/icons/Vector.svg'>
+                a.link(href='#') Neutrino
+                <img class='click' src='~static/img/icons/tap-copy-6.svg'>
+          .sponsor-item
+            <img class='img-sponsors' src='img/band.svg', alt='Band'>
+            .cotainer-about
+              .about-sponsor
+                <img class='img-sponsors' src='img/band.svg', alt='Band'>
+                .text-about Swingby provides technology for inter-chain communications based on MPC (multiparty computations), TSS (threshold signatures) and ZK (zero-knowledge). This approach works with any chain and is most helpful with blockchains that lack Turing-complete smart contracts functionality.
+                .status Status:
+                  .status-type &nbsp; Validator &nbsp; <img src='~static/img/icons/Vector.svg'>
+                a.link(href='#') Band
+                <img class='click' src='~static/img/icons/tap-copy-6.svg'>
+          .sponsor-item
+            <img class='img-sponsors' src='img/vid.svg', alt='Vid'>
+            .cotainer-about
+              .about-sponsor
+                <img class='img-sponsors' src='img/vid.svg', alt='Vid'>
+                .text-about Swingby provides technology for inter-chain communications based on MPC (multiparty computations), TSS (threshold signatures) and ZK (zero-knowledge). This approach works with any chain and is most helpful with blockchains that lack Turing-complete smart contracts functionality.
+                .status Status:
+                  .status-type &nbsp; Validator &nbsp; <img src='~static/img/icons/Vector.svg'>
+                a.link(href='#') Vid
+                <img class='click' src='~static/img/icons/tap-copy-6.svg'>
+</template>
+
+<script>
+import SponsorBlock from '~/components/index/Sponsor.vue';
+
+export default {
+  components: SponsorBlock,
+};
+</script>
+
+
+
+<style lang="scss" scoped>
+@import '../../assets/scss/mixins/media.scss';
+
+section.sponsor-wrap {
+  z-index: 22;
+  position: relative;
+  mix-blend-mode: normal;
+  //height: 600px;
+  display: flex;
+  justify-content: center;
+
+  @include bb(tablet, 0) {
+    padding: 40px 20px;
+  }
+
+  @include b(mobile) {
+    padding: 20px 20px;
+  }
+
+  @include bb(endmobile, 0) {
+    padding: 40px 20px;
+  }
+}
+
+.container-content-logo {
+  max-width: 1042px;
+  width: 100%;
+  height: 376px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  margin-top: 89px;
+
+  .block-sponsor-gravity {
+    background: url('~static/img/bg-title-watching.svg') no-repeat center;
+    width: 1042px;
+    height: 376px;
+
+    .title-text {
+      font-family: Orbitron;
+      font-style: normal;
+      font-weight: 900;
+      font-size: 44px;
+      line-height: 54px;
+      letter-spacing: 4.032px;
+      color: #fafafa;
+      mix-blend-mode: normal;
+      opacity: 0.8;
+      text-align: center;
+      margin-bottom: 72px;
+
+      @include bb(endmobile, 0) {
+        font-size: 20px;
+        margin-bottom: 20px;
+      }
+    }
+  }
+  .container-img-sponsors {
+    .block {
+      display: flex;
+      justify-content: space-between;
+      .sponsor-item {
+        position: relative;
+        z-index: 10;
+        display: flex;
+        position: relative;
+        justify-content: space-between;
+        width: 248px;
+        height: 73px;
+        margin: 0 14px 14px 0;
+        border: 1px solid #22243b;
+
+        &:hover {
+          .cotainer-about {
+            display: block;
+            z-index: 20;
+          }
+        }
+
+        .img-sponsors {
+          margin: auto;
+        }
+
+        .cotainer-about {
+          display: none;
+          .about-sponsor {
+            z-index: 20;
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            position: absolute;
+            left: -1px;
+            top: -1px;
+            width: 246px;
+            height: 348px;
+            background: #141623;
+            mix-blend-mode: normal;
+            border: 1px solid #22243b;
+            box-sizing: border-box;
+            padding-top: 12px;
+          }
+          .text-about {
+            height: 190px;
+            width: 206px;
+            font-family: Poppins;
+            font-style: normal;
+            font-weight: 300;
+            font-size: 12px;
+            line-height: 19px;
+            color: #ffffff;
+            mix-blend-mode: normal;
+            opacity: 0.74;
+            margin: auto;
+          }
+          .status {
+            display: flex;
+            font-family: Poppins;
+            font-style: normal;
+            font-weight: 300;
+            font-size: 12px;
+            line-height: 20px;
+            color: #ffffff;
+            mix-blend-mode: normal;
+            margin: 0 0 9px 20px;
+          }
+          .status-type {
+            color: #44d7b6;
+            background-image: url('~static/img/icons/Vector.svg') no-repeat;
+          }
+        }
+        .link {
+          font-family: Poppins;
+          font-style: normal;
+          font-weight: normal;
+          font-size: 12px;
+          line-height: 20px;
+          text-decoration-line: underline;
+          color: #ffffff;
+          mix-blend-mode: normal;
+          opacity: 0.74;
+          text-decoration: none;
+          margin: 0 0 24px 20px;
+        }
+        .click {
+          position: absolute;
+          top: 12px;
+          right: 11px;
+        }
+      }
+    }
+  }
+}
+</style>
