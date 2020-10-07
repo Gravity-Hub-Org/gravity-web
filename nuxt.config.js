eb3466e1699fb72383da7298c88757a0a0a2b5ca
PoulWalcker <pashasimanko123@gmail.com>
2020-10-07 22:10:13 +0300

    fix nuxt-config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,55 +1,71 @@
 const { sections, metaTags, flattenSections, shortDescription, getSitemap } = require('./src/global/links');
 
-export const head = {
-  title: shortDescription,
-  meta: metaTags,
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico',
-    },
-    {
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css?family=Orbitron:400,500,600,700,800,900|Poppins:100,200,300,400,500,600,700,800,900&display=swap',
-    },
-  ],
-};
-export const loading = {
-  color: '#ff8e1e',
-};
-export const build = {
+module.exports = {
   /*
-   ** Run ESLint on save
+   ** Headers of the page
    */
-  extend(config, { isDev, isClient }) {
-    if (isDev && isClient) {
-      config.module.rules.push({
-        enforce: 'pre',
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        exclude: /(node_modules)/,
-      });
+  head: {
+    title: shortDescription,
+    meta: metaTags,
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Orbitron:400,500,600,700,800,900|Poppins:100,200,300,400,500,600,700,800,900&display=swap',
+      },
+    ],
+  },
+  /*
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#ff8e1e',
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** Run ESLint on save
+     */
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        });
+      }
+    },
+  },
+  srcDir: 'src/',
+  modules: [
+    '@aceforth/nuxt-optimized-images',
+    '@nuxtjs/sitemap',
+    // ['@nuxtjs/google-tag-manager', { id: 'UA-168552658-1' }],
+    // '@nuxtjs/gtm'
+  ],
+  buildModules: [
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-168552658-2'
+    }]
+  ],
+  router: {
+    generate: {
+      routes: ['whitepaper']
     }
   },
+
+  optimizedImages: {
+    optimizeImagesInDev: true,
+    optimizeImages: true,
+  },
+
+  sitemap: getSitemap(),
 };
-export const srcDir = 'src/';
-export const modules = [
-  '@aceforth/nuxt-optimized-images',
-  '@nuxtjs/sitemap',
-];
-export const buildModules = [
-  ['@nuxtjs/google-analytics', {
-    id: 'UA-168552658-2'
-  }]
-];
-export const router = {
-  generate: {
-    routes: ['whitepaper']
-  }
-};
-export const optimizedImages = {
-  optimizeImagesInDev: true,
-  optimizeImages: true,
-};
-export const sitemap = getSitemap();
