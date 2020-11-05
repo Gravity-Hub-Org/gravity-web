23a4a0debb4381eb4790017d0ba8f2916795f85e
PoulWalcker <pashasimanko123@gmail.com>
2020-11-05 15:04:16 +0300

    fix nav block

--- src/components/index/Menu.vue
+++ src/components/index/Menu.vue
@@ -112,6 +112,7 @@ export default {
         flex-direction: column;
         overflow-y: scroll;
         overflow-x: hidden;
+        justify-content: space-between;
       }
     }
 
@@ -120,7 +121,9 @@ export default {
         display: flex;
         flex-direction: column;
         justify-content: flex-end;
-        margin-top: 200px;
+        @include bb(endmobile, 0) {
+          margin-top: 20px;
+        }
 
         &-bottom {
           img {
