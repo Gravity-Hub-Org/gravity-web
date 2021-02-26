c21d5bad6f41d443fedefe156880591551329b50
PoulWalcker <pashasimanko123@gmail.com>
2021-02-26 16:58:51 +0300

    add redirect mm-waves

---
+++ src/pages/demo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { getDemoMM, sections } from '../global/links';
+export default {
+  fetch(params) {
+    const { redirect } = params;
+    redirect(301, getDemoMM(sections))
+  },
+};
+</script>
